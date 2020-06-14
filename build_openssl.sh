f28636d54e6021a4779440567169b1530042018f
Wojtek Mach <wojtek@wojtekmach.pl>
2020-06-14 13:13:01 +0200

    Initial commit

---
+++ build_openssl.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo Usage:
+  echo
+  echo "    ./build_openssl.sh VERSION"
+  echo 
+  echo Examples:
+  echo
+  echo "    ./build_openssl.sh 1.1.1g"
+  echo
+  exit 1
+fi
+
+set -euox pipefail
+
+wd=$PWD
+openssl_version=$1
+elixirup_dir=/tmp/elixirup
+src_root_dir=$elixirup_dir/src/openssl
+src_dir=${src_root_dir}/${openssl_version}
+dest_root_dir=$elixirup_dir/installs/openssl
+dest_dir=${dest_root_dir}/${openssl_version}
+archive_root_dir=$elixirup_dir/archives
+archive_path=${archive_root_dir}/openssl-${openssl_version}-macos.tar.gz
+
+mkdir -p $src_root_dir
+mkdir -p $dest_root_dir
+mkdir -p $archive_root_dir
+
+if [ ! -d "${src_dir}" ]; then
+  cd $src_root_dir
+  curl -C - -O https://www.openssl.org/source/openssl-${openssl_version}.tar.gz
+  tar xvzf openssl-${openssl_version}.tar.gz
+  mv openssl-${openssl_version} $src_dir
+fi
+
+if [ ! -d "${dest_dir}" ]; then
+  cd $src_dir
+  ./config --prefix=$dest_dir
+  make
+  make install_sw
+fi
+
+if [ ! -f $archive_path ]; then
+  cd $dest_root_dir
+  tar czf $archive_path ${openssl_version}/
+fi
+
+cd $wd
---
+++ build_otp.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo Usage:
+  echo
+  echo "    ./build_otp.sh VERSION"
+  echo 
+  echo Examples:
+  echo
+  echo "    ./build_otp.sh 23.0.2"
+  echo
+  exit 1
+fi
+
+set -euox pipefail
+
+wd=$PWD
+otp_version=$1
+elixirup_dir=/tmp/elixirup
+src_root_dir=$elixirup_dir/src/otp
+src_dir=${src_root_dir}/${otp_version}
+dest_root_dir=$elixirup_dir/installs/otp
+dest_dir=${dest_root_dir}/${otp_version}
+archive_root_dir=$elixirup_dir/archives
+archive_path=${archive_root_dir}/otp-${otp_version}-macos.tar.gz
+
+mkdir -p $src_root_dir
+mkdir -p $dest_root_dir
+mkdir -p $archive_root_dir
+
+if [ ! -d "${src_dir}" ]; then
+  cd $src_root_dir
+  ref=OTP-${otp_version}
+  curl -L -O https://github.com/erlang/otp/archive/${ref}.tar.gz
+  ls
+  tar xvzf ${ref}.tar.gz
+  mv otp-${ref} $src_dir
+fi
+
+if [ ! -d "${dest_dir}" ]; then
+  openssl_dir="${elixirup_dir}/installs/openssl/1.1.1g"
+  export RELEASE_ROOT=$dest_dir
+  cd $src_dir
+  ./otp_build autoconf
+
+  ./configure --with-ssl="${openssl_dir}" --enable-dirty-schedulers --enable-builtin-zlib --without-javac
+
+  make -j$(getconf _NPROCESSORS_ONLN)
+  make release
+  make release_docs DOC_TARGETS="chunks"
+  make install
+fi
+
+if [ ! -f $archive_path ]; then
+  cd $dest_root_dir
+  tar czf $archive_path ${otp_version}/
+fi
+
+cd $wd
+
+# # post-install
+# ./Install -sasl $PWD
